d2377cefa31901da24979db5fafe0ffb4440ae29
David Fraser <davidf@inconceivable.co.za>
2025-04-15 10:13:45 +0000

    Back up each database to a separate file

--- scripts/backup-ghosts.sh
+++ scripts/backup-ghosts.sh
@@ -8,7 +8,11 @@ mkdir -p $BACKUP_DIR
 # Back up MySQL
 echo "Backing up all databases..."
 MYSQL_ROOT_PASSWORD=$(grep MYSQL_ROOT_PASSWORD ghosts-toaster.env | cut -d= -f2)
-docker exec mysql mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --all-databases > $BACKUP_DIR/all-databases.sql
+databases=`docker exec mysql mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES;" | grep -v "Database\|information_schema\|performance_schema\|mysql\|sys"`
+for database in $databases; do
+  echo "Backing up database: $db"
+  docker exec mysql mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" "$db" > "$BACKUP_DIR/$db.sql"
+done
 
 # Back up Ghost content
 echo "Backing up all Ghost content..."
