27ba86b56487bee6196f00fbeff7f9538146327f
David Fraser <davidf@inconceivable.co.za>
2025-04-23 15:15:41 +0000

    Use wpull so we can use its plugins

--- webhook/webhook-server.js
+++ webhook/webhook-server.js
@@ -56,7 +56,7 @@ function generateStaticSite(siteName, siteDomain) {
       }
     }
     
-    const command = `docker exec static-generator gssg --domain http://ghost_${siteName}:2368 --productionDomain https://${siteDomain} --dest ${outputDir} --avoid-https`;
+    const command = `docker exec static-generator gssg --domain http://ghost_${siteName}:2368 --productionDomain https://${siteDomain} --dest ${outputDir} --avoid-https --useWpull`;
     
     console.log(`Executing command: ${command}`);
     
