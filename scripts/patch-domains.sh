b8fc233afa1a6d9fd38e46bbcf217b81db32b46f
David Fraser <davidf@inconceivable.co.za>
2025-11-17 12:21:41 +0000

    Give this more intelligence about its location, and pass a site name rather than directory
    Then use the environment to determine the prefixes rather than hardcoding

--- scripts/patch-domains.sh
+++ scripts/patch-domains.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# Script patch domain names for a Ghost mirror directory
 
-# Function to generate static site for a Ghost instance
+script_dir="$(cd "$(dirname "$0")"; pwd)"
+root_dir="$(cd "$(dirname "$script_dir")"; pwd)"
+
+SITES_DIR="$root_dir/sites"
+STATIC_DIR="$root_dir/static"
+SG_STATIC_DIR="/static"
+GLOBAL_ENV_FILE="$root_dir/.env"
+
+# Load global environment variables
+if [ -f "$GLOBAL_ENV_FILE" ]; then
+    source "$GLOBAL_ENV_FILE"
+fi
+
+# Function to patch domain names for a Ghost mirror directory
 patch_static_site() {
-    local site_dir=$1
-    local env_file="$site_dir/site.env"
+    local site_name=$1
+    local env_file="$SITES_DIR/$site_name/site.env"
     
     # Check if site.env file exists
     if [ ! -f "$env_file" ]; then
@@ -24,7 +38,9 @@ patch_static_site() {
     echo "Patching static site for $site_domain..."
 
     # this should support https:// http:// "// '// or ref= as prefixes that if matched with the domain name should be adjusted
-    find "$local_output_dir" -type f -exec sed -i 's#\(https://\|http://\|["'"'"']//\|ref=\)ghost[.]'"$SITE_DOMAIN"'#\1toast.'"$SITE_DOMAIN#g" "{}" +
+    GHOST_DOMAIN_SEARCH="${GHOST_PREFIX}${GHOST_PREFIX:+[.]}${SITE_DOMAIN//./[.]}"
+    STATIC_DOMAIN_REPLACE="${STATIC_PREFIX}${STATIC_PREFIX:+.}${SITE_DOMAIN}"
+    find "$local_output_dir" -type f -exec sed -i 's#\(https://\|http://\|["'"'"']//\|ref=\)'"$GHOST_DOMAIN_SEARCH"'#\1'"$STATIC_DOMAIN_REPLACE#g" "{}" +
 }
 
 # this can be run as an executable script, or sourced so the function can be used
